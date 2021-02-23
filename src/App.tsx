b8a62e4ec45c56ccbbb9595144f81fe36668fe3b
Bruno Santos <bm-santos@outlook.com.br>
2021-02-23 01:11:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { isPropertySignature } from "typescript";
+import { ExperienceBar } from "./components/ExperienceBar";
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
